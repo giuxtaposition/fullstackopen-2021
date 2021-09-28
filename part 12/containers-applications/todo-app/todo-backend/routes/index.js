7bb7a0d5adabf018a9d24acf0683bbbf997c3d00
giuxtaposition <yg97.cs@gmail.com>
2021-09-28 23:28:02 +0200

    part 12 b

--- part 12/containers-applications/todo-app/todo-backend/routes/index.js
+++ part 12/containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const redis = require('../redis')
+const router = express.Router()
 
 const configs = require('../util/config')
 
@@ -11,8 +12,16 @@ router.get('/', async (req, res) => {
 
   res.send({
     ...configs,
-    visits
-  });
-});
+    visits,
+  })
+})
 
-module.exports = router;
+router.get('/statistics', async (req, res) => {
+  const counter = await redis.getAsync('counter')
+
+  res.send({
+    added_todos: counter ? counter : 0,
+  })
+})
+
+module.exports = router
